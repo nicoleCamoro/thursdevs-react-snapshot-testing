a3fc95d88b27b11cf8e66363477373341a294783
franchette <franchette@sym.ph>
2018-09-27 12:32:11 +0800

    Ignore cache folder

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -22277,7 +22277,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61386" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64196" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
