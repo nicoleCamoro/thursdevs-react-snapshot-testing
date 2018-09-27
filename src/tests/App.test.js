8518a15e4cc9a7bf54cfff831da7497bce7314cf
franchette <franchette@sym.ph>
2018-09-27 17:11:05 +0800

    Add tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,38 @@
-function sum(a, b) {
-    return a + b;
-}
+import React from 'react';
+import renderer from 'react-test-renderer';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
+import Button from '../react/components/Button';
+import DogMedia from '../react/components/DogMedia';
+
+// describe('Button component', () => {
+//     it('Button components renders initially', () => {
+//         const component = renderer.create(<Button onClick={() => {}} />);
+
+//         expect(component.toJSON()).toMatchSnapshot();
+//     });
+
+//     it('renders with null as an initial state', () => {
+//         const component = renderer.create(<Button onClick={() => {}} />);
+//         const instance = component.getInstance();
+//         // console.log(instance);
+
+//         expect(instance.state).toBeNull();
+//     });
+// });
+
+// describe('DogMedia component', () => {
+//     it('renders video', () => {
+//         const component = renderer.create(<DogMedia mediaType="video" />);
+//         const instance = component.getInstance();
+
+//         expect(instance.props.mediaType).toBe('video');
+//     });
+// });
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+// });
